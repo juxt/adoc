7d40d87d1ed991c93ebaa0123ad02258c744e40c
reborg <reborg@reborg.net>
2016-08-15 17:27:12 +0100

    using git tags for versioning

--- project.clj
+++ project.clj
@@ -1,4 +1,22 @@
-(defproject juxt/adoc "0.1.7"
+(require '[clojure.java.shell :as sh])
+
+(defn next-version [version]
+  (when version
+    (let [[a b] (next (re-matches #"(.*?)([\d]+)" version))]
+      (when (and a b)
+        (str a (inc (Long/parseLong b)))))))
+
+(defn version []
+  (let [[version commits hash dirty?]
+        (next (re-matches #"(.*?)-(.*?)-(.*?)(-dirty)?\n"
+                          (:out (sh/sh "git" "describe" "--dirty" "--long" "--tags" "--match" "[0-9].*"))))]
+    (if (or dirty? (pos? (Long/parseLong commits)))
+      (str (next-version version) "-SNAPSHOT")
+      version)))
+
+(def +version+ (version))
+
+(defproject juxt/adoc +version+
   :description "Clojure wrapper around AsciidoctorJ"
   :url "https://github.com/juxt/adoc"
   :license {:name "Eclipse Public License - v 1.0"
